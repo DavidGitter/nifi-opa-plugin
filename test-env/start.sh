8d701e539d65ca0f24bddd44fced407546aee12c
DavidGitter <80157809+DavidGitter@users.noreply.github.com>
2025-11-29 16:24:59 +0100

    added restart to start.sh for test-env

--- test-env/start.sh
+++ test-env/start.sh
@@ -3,3 +3,6 @@ docker compose up -d
 sleep 5s
 echo "### Inserting authorizers.xml into Apache NiFi Container..."
 docker cp ./authorizers-opa.xml test-env-nifi-1:/opt/nifi/nifi-current/conf/authorizers.xml
+echo "### Restarting Apache NiFi Container to activate new authorizers.xml..."
+docker restart test-env-nifi-1
+echo "### Finished setting up test environment."
